b38c0979b72c7b68c8d25c7365816c623629147b
Jennifer Fox <jenniferelizfox@gmail.com>
2024-08-14 01:17:20 +0100

    Vite package now in use
    File changes because of transfer to Vite package.

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import Game from "./Game"
+
+// Overall Screen that displays header and Game
+//const game character length
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          You have 6 attempts to guess the correct 5 letter word.
+        </p>
+      </header>
+      <div>
+      <Game className='Game'/>
+      </div>
+    </div>
+  );
+};
