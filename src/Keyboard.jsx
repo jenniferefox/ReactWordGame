c5ab79bfeabed48f6cf6b0ef2f1949ef8eb09114
Jennifer Fox <jenniferelizfox@gmail.com>
2024-08-19 23:29:20 +0100

    Function added to update key colours after guess
    - KEYCOL map updated with letters guessed alongside correct/incorrect/almost status.
    - Keyboard buttons changed to divs to remove bug where keyboard inputs letter upon 'enter' when cursor is over software keyboard

--- src/Keyboard.jsx
+++ src/Keyboard.jsx
@@ -1,14 +1,16 @@
 import { useEffect } from 'react';
+import checkAgainstAnswer from './checkAgainstAnswer'
 //Keyboard component structures the on-screen keyboard and takes input from
 // physical and software keyboard.
 
 export default function Keyboard (props) {
+  //handles input
   useEffect(() => {
     const handler = (e) => {
       if (e.key === 'Backspace' || e.key === 'Delete') {
         props.onKey('Backspace')
       } else if (e.key === 'Enter') {
-        props.onKey("Enter")
+        props.onKey('Enter')
       } else if (e.key.match(/[a-zA-Z]/)) {
         props.onKey(e.key.toUpperCase())
       };
@@ -20,6 +22,8 @@ export default function Keyboard (props) {
       }
     }, [props.onKey]);
 
+  const keyColours = keyColourUpdate(props.prevGuesses, props.answerWord);
+
   const keys = [
     ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
     ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
@@ -29,18 +33,34 @@ export default function Keyboard (props) {
   return keys.map((row, index) => {
     return (
       <div className='key-row' key={index}>
-        {row.map((keyValue) => <Key key={keyValue} legend={keyValue} onKey={props.onKey}/>)}
+        {row.map((keyValue) => <Key key={keyValue} legend={keyValue} keyColours={keyColours} onKey={props.onKey}/>)}
       </div>
     )
   });
 }
 
+function keyColourUpdate(prevGuesses, answerWord) {
+  const KEYCOL = new Map()
+  for (let k = 0; k < prevGuesses.length; k++) {
+    const letterCheck = checkAgainstAnswer(prevGuesses[k], answerWord);
+    for (let l = 0; l < letterCheck.length; l++) {
+      if (!(KEYCOL.get(prevGuesses[k][l]))) {
+        KEYCOL.set(prevGuesses[k][l], letterCheck[l])
+      } else if ((KEYCOL.get(prevGuesses[k][l]) === 'almost' || KEYCOL.get(prevGuesses[k][l]) === 'false') && letterCheck[l] === 'true') {
+        KEYCOL.set(prevGuesses[k][l], letterCheck[l])
+      };
+      };
+    };
+  return KEYCOL;
+}
+
 // Key component handles each Key in the Keyboard
 
 function Key(props) {
+  const colour = (props.keyColours).get(props.legend)
   return (
-    <button className='key' onClick={() => {props.onKey(props.legend)}}>
+    <div className={`key ${colour}`} onClick={() => {props.onKey(props.legend)}}>
     {props.legend}
-    </button>
+    </div>
   )
 };
